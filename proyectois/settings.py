af00515197760ab0088494e248abecfff1b26711
AndRodriguez <anrodrigueza@unitru.edu.pe>
2024-12-05 17:41:20 -0500

    config s

--- proyectois/settings.py
+++ proyectois/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 ]
 
 
-CSRF_TRUSTED_ORIGINS = ["sisweb-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://sisweb-production.up.railway.app"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
